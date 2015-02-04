eeae092512e01bfeacf329452f14878b074dab9f
codeBelt <code@codebelt.com>
2015-02-04 08:18:54 -0600

    add gulp and psd

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+//http://dinozafirakos.com/2014/06/30/typescript-and-gulp/
+
+var gulp   = require('gulp');
+var tsc    = require('gulp-tsc');
+var shell  = require('gulp-shell');
+var runseq = require('run-sequence');
+
+var paths = {
+    tscripts : {
+        src : [
+            './server.ts',
+            'app/**/*.ts',
+            'public/**/*.ts',
+            'Scripts/**/*.ts'
+        ],
+        dest : './'
+    }
+};
+
+gulp.task('default', ['buildrun']);
+
+// Run
+
+gulp.task('run', shell.task([
+    'node ./server.js'
+]));
+
+gulp.task('buildrun', function (cb) {
+    runseq('build', 'run', cb);
+});
+
+// Compile
+
+gulp.task('build', ['compile:typescript']);
+gulp.task('compile:typescript', function () {
+    return gulp
+        .src(paths.tscripts.src)
+        .pipe(tsc({
+            module: "CommonJS",
+            sourcemap: true,
+            emitError: false
+        }))
+        .pipe(gulp.dest(paths.tscripts.dest));
+});
